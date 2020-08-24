8bf7ec976ecc16c08c4d63c190d7b7ee35b39db5
Bhupendra381 <68839738+Bhupendra381@users.noreply.github.com>
2020-08-24 11:14:24 +0530

    Update Theme.js

--- src/constants/Theme.js
+++ src/constants/Theme.js
@@ -12,4 +12,35 @@ const theme = {
 	}
 };
 
+
 export default theme;
+
+const lightTheme = {
+	dimmer: "#9B9B9B",
+	dim: "#008080",
+	medium: "#ffffff",
+	bright: "#f0f8ff",
+	primary: "#0000ff",
+	danger: "#EB7680",
+	text: "#0000ff",
+	gradient: {
+		start: "#5f9ea0",
+		end: "#a9a9a9",
+	}
+};
+
+const darkTheme = {
+	dimmer: "#9B9B9B",
+	dim: "#ffff00",
+	medium: "#121212",
+	bright: "#121212",
+	primary: "#6200EE",
+	danger: "#EB7680",
+	text: "#ffffff",
+	gradient: {
+		start: "#CB00FC",
+		end: "#9B00F8",
+	}
+};
+
+export { lightTheme, darkTheme };
