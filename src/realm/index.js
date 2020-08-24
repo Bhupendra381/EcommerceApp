2b6531ecc1ce08896fbc6f9737b0f30b06fc02d6
Bhupendra381 <68839738+Bhupendra381@users.noreply.github.com>
2020-08-24 11:17:25 +0530

    Update index.js

--- src/realm/index.js
+++ src/realm/index.js
@@ -2,10 +2,11 @@ import * as Realm from "realm";
 import userSchema from "./userSchema";
 import productSchema from "./productSchema";
 import promoSchema from "./promoSchema";
+import themeSchema from '../realm/themeSchema';
 
 function connect(cb) {
 	Realm.open({
-		schema: [userSchema, productSchema, promoSchema],
+		schema: [userSchema, productSchema, themeSchema, promoSchema],
 		schemaVersion: 5,
 	}).then(cb);
 }
